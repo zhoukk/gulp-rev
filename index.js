51ea2d099084ea2c8c6257aca9a51d0e90987616
zhoukk <izhoukk@gmail.com>
2017-08-10 09:53:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ plugin.manifest = (pth, opts) => {
 		const revisionedFile = relPath(file.base, file.path);
 		const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
 
-		manifest[originalFile] = revisionedFile;
+		manifest[originalFile] = originalFile + '?v=' + file.revHash;
 
 		cb();
 	}, function (cb) {
